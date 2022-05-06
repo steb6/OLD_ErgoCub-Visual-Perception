641e366349674962dea8505c73f8cb755ff6e009
andrearosasco <andrea.rosasco@iit.it>
2022-05-06 10:50:08 +0200

    added multiprocessing framework

---
+++ utils/multiprocessing/__init__.py
@@ -0,0 +1,4 @@
+from utils.multiprocessing.node import Node
+from utils.multiprocessing.network import Network
+from utils.multiprocessing.signals import Signals
+from utils.multiprocessing.data_manager import DataManager
---
+++ utils/multiprocessing/signals.py
@@ -0,0 +1,6 @@
+
+
+class Signals:
+    OK = 0
+    READY = 1
+    STOP = 2
